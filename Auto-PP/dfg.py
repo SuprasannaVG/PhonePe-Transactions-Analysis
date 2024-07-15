b32a3c99f1443b1daf28203f806640004bfebd7e
Suprasanna V Gunaga <125822020+SuprasannaVG@users.noreply.github.com>
2024-07-15 23:03:59 +0530

    Delete Auto-PP directory

--- Auto-PP/dfg.py
+++
@@ -1,6 +0,0 @@
-import tabula
-inp=(r"uploads/PhonePe_Statement_May2024_Jun2024.pdf")
-out=(r"test.csv")
-
-df=tabula.read_pdf(input_path=inp, pages="all")
-tabula.convert_into(input_path=inp,output_path=out, output_format="csv",pages="all",stream=True)
